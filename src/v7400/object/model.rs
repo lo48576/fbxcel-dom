1fee564ea66d744edb8e29f0eedd6c9b7f56c4b9
John Austin <kleptine@gmail.com>
2021-03-14 20:16:29 -0600

    Adds a couple of useful functions for working with models.

--- src/v7400/object/model.rs
+++ src/v7400/object/model.rs
@@ -6,6 +6,8 @@ pub use self::{
     camera::CameraHandle, light::LightHandle, limbnode::LimbNodeHandle, mesh::MeshHandle,
     null::NullHandle,
 };
+use mint::Vector3;
+use fbxcel::low::v7400::AttributeValue;
 
 mod camera;
 mod light;
@@ -56,5 +58,57 @@ impl<'a> ModelHandle<'a> {
                 TypedObjectHandle::Model(o) => Some(o),
                 _ => None,
             })
+    }
+
+    /// Gets the root-most model parent of this object in the scene hierarchy.
+    ///
+    /// If this object has no parents, returns this object.
+    ///
+    /// There may be multiple root models in a single scene.
+    pub fn root_model<'b>(&'b self) -> TypedModelHandle<'a> {
+        let mut parent : TypedModelHandle = match self.get_typed() {
+            TypedObjectHandle::Model(o) => o,
+            _ => panic!("ModelHandle should always have TypedObjectHandle::Model type.")
+        };
+
+        while let Some(m) = parent.parent_model() {
+            parent = m;
+        }
+        return parent;
+    }
+
+    /// Returns the local rotation (Lcl Rotation) of this model object, if one is present.
+    pub fn local_rotation(&self) -> Option<Vector3<f64>> {
+        let rotation = self.direct_properties()?.get_property("Lcl Rotation")?;
+
+        // Pull out the first three float attributes.
+        let components: Vec<f64> = rotation.node().attributes().iter().filter_map(|attr| {
+            return match attr {
+                AttributeValue::F64(value) => Some(value.clone()),
+                _ => None
+            }
+        }).collect();
+
+        // todo: Is it ever possible for a rotation to return a quaternion (ie. 4 values)?
+        assert_eq!(components.len(), 3);
+
+        return Some(Vector3::from_slice(components.as_slice()));
+    }
+
+    /// Returns the local scale (Lcl Scale) of this model object, if one is present.
+    pub fn local_scale(&self) -> Option<Vector3<f64>> {
+        let rotation = self.direct_properties()?.get_property("Lcl Scaling")?;
+
+        // Pull out the first three float attributes.
+        let components: Vec<f64> = rotation.node().attributes().iter().filter_map(|attr| {
+            return match attr {
+                AttributeValue::F64(value) => Some(value.clone()),
+                _ => None
+            }
+        }).collect();
+
+        assert_eq!(components.len(), 3);
+
+        return Some(Vector3::from_slice(components.as_slice()));
     }
 }
